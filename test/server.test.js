a52391e40da8ee3d536c9bec3059dc10ed20d78c
3rick <erick.dejesus1@alumno.buap.mx>
2022-05-15 08:27:21 -0500

    test: upd server test passed

--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,7 @@
 describe('Unit tests for server.js ', ()=>{
     test('1) test endpoints', () => {
         const experimental = 1;
-        expect(experimental).toBe(2)
+        expect(experimental).toBe(1)
     })
 
 })
