345329e0e3a6d5c7f6b4530ae58ad1181c61b866
3rick <erick.dejesus1@alumno.buap.mx>
2022-05-15 17:42:59 -0500

    chore: add prisma seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,59 +3,76 @@ const prisma = new PrismaClient();
 
 (async function main() {
     try {
-        const seaTurtle = await prisma.animal.upsert({
-            where: { name: 'SEA TURTLE' },
+        const totoaba = await prisma.animal.upsert({
+            where: { name: 'Totoaba' },
             update: {},
             create: {
-                name: 'SEA TURTLE',
-                scientificName: 'Cheloniidae and Dermochelyidae families',
-                type: 'null',
-                amountAlive: 10,
-                info: 'Over the last 200 years, human activities have tipped the scales against the survival of these ancient mariners. Slaughtered for their eggs, meat, skin, and shells, sea turtles suffer from poaching and over-exploitation',
-                location: 'Open water and coasts',
-                image: 'url'
+                name: 'Totoaba',
+                scientificName: 'Totoaba macdonaldi',
+                type: 'Sciaenidae',
+                amountAlive: 2300,
+                info: 'también es conocida como corvina blanca, curvinas, roncadores o tambores por su habilidad de producir sonidos con su vejiga natatoria. Es un pez muy longevo, pues llega a vivir de los 25 hasta los 50 años de edad.',
+                location: 'Alto Golfo de California',
+                image:'../../../../../assets/img/01.png'
+
             },
         });
 
-        const animal2 = await prisma.animal.upsert({
-            where: { name: 'animal2' },
+        const tortugaCarey = await prisma.animal.upsert({
+            where: { name: 'Tortuga carey' },
             update: {},
             create: {
-                name: 'animal2',
-                scientificName: 'cientific_name',
-                type: 'show_type',
-                amountAlive: 9,
-                info: 'info_text',
-                location: 'location_text',
-                image: 'url'
+                name: 'Tortuga carey',
+                scientificName: 'Eretmochelys imbricata',
+                type: 'Reptil',
+                amountAlive: 8000,
+                info: 'Las tortugas carey, que no son particularmente grandes comparadas con otras tortugas marinas, llegan a medir hasta 114 centímetros respecto del tamaño de su caparazón y a pesar 68 kilogramos.',
+                location: 'Aguas tropicales de los océanos Atlántico, Pacífico e Índico.',
+                image:'../../../../../assets/img/02.png'
+
             },
         });
 
-        const animal3 = await prisma.animal.upsert({
-            where: { name: 'animal3' },
+        const vaquitaMarina = await prisma.animal.upsert({
+            where: { name: 'Vaquita marina' },
             update: {},
             create: {
-                name: 'animal3',
-                scientificName: 'cientific_name',
-                type: 'show_type',
-                amountAlive: 9,
-                info: 'info_text',
-                location: 'location_text',
-                image: 'url'
+                name: 'Vaquita marina',
+                scientificName: 'Phocoena sinus',
+                type: 'Cetáceo',
+                amountAlive: 30,
+                info: 'Mide 150 cm de largo y pesa hasta 50 kg. Tiene una distribución muy restringida.',
+                location: 'Norte del Golfo de California',
+                image:'../../../../../assets/img/03.png'
+
             },
         });
 
-        const animal4 = await prisma.animal.upsert({
-            where: { name: 'animal4' },
+        const BallenaAzul = await prisma.animal.upsert({
+            where: { name: 'Ballena azul' },
             update: {},
             create: {
-                name: 'animal4',
-                scientificName: 'cientific_name',
-                type: 'show_type',
-                amountAlive: 9,
-                info: 'info_text',
-                location: 'location_text',
-                image: 'url'
+                name: 'Ballena azul',
+                scientificName: 'Balaenoptera musculus',
+                type: 'Cetáceo',
+                amountAlive: 2000,
+                info: 'Su tamaño medio es de entre 24 y 27 metros de longitud y pesan entre 100 y 120 toneladas, aunque hay registros de ejemplares de más de 30 m de longitud y 170 t de peso.',
+                location: 'Atlantico Norte, Pasifico, Indico y Antartico',
+                image:'../../../../../assets/img/04.png'
+            },
+        });
+
+        const tiburónMartilloGigante = await prisma.animal.upsert({
+            where: { name: 'Tiburón Martillo Gigante' },
+            update: {},
+            create: {
+                name: 'Tiburón Martillo Gigante',
+                scientificName: 'Sphyrna mokarran',
+                type: 'Carcharhiniformes',
+                amountAlive: 2000,
+                info: 'Llega a medir de 0.9 hasta 6 metros y pesa cerca de 580 kg. Es conocido como uno de los depredadores más agresivos del planeta.',
+                location: 'Zonas del Golfo de México.',
+                image:'../../../../../assets/img/05.png'
             },
         });
 
